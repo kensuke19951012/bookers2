faf2cb4d362438bafd20434d34ff1d8bed7e4ed2
EC2 Default User <ec2-user@ip-172-31-59-207.us-west-2.compute.internal>
2022-06-28 11:35:05 +0000

    finish

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,66 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+  end
+
+  def show
+    @newbook = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+  
+  def edit
+  @book = Book.find(params[:id])
+    if @book.user == current_user
+        render "edit"
+    else
+        redirect_to books_path
+    end
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @books = Book.all
+    @user = current_user
+  if @book.save 
+    flash[:notice]="Book was successfully created."
+    redirect_to book_path(@book.id)
+  else
+     render :index
+  end
+  end
+  
+   def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+      flash[:notice]="Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+   end
+
+ def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice]="Book was successfully destroyed."
+      redirect_to books_path
+    end
+ end
+
+
+ private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+
+
+end
---
+++ db/migrate/20220628084355_change_introduction_to_users.rb
@@ -0,0 +1,10 @@
+class ChangeIntroductionToUsers < ActiveRecord::Migration[6.1]
+def up
+    change_column_null :users, :introduction, true
+end
+
+def down
+    change_column_null :users, :introduction, false
+end
+ 
+end
